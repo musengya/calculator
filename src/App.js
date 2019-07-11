33b0ea08dc5ed3460af7b2bd2cca270fb16096bd
Irene Mutua <mutuamirene@gmail.com>
2019-07-11 14:34:48 +0300

    UUpdate the App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: ""
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(e) {
+    
+  }
   render() {
     return (
       <div>
-        
+        <h1>Simple Calculator</h1>
       </div>
     )
   }
