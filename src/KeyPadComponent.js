627884624855b0e36d0918f8c7023c6fd00d5c22
Irene Mutua <mutuamirene@gmail.com>
2019-07-15 07:53:38 +0300

    Initial Commit

---
+++ src/KeyPadComponent.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class KeyPadComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+
+}
+
+export default KeyPadComponent
