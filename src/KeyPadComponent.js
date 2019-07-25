3e8c525a599b0ea28928e4d81d097cc1ac6a8e54
Irene Mutua <mutuamirene@gmail.com>
2019-07-25 11:18:21 +0300

    Change keypad component

--- src/KeyPadComponent.js
+++ src/KeyPadComponent.js
@@ -5,16 +5,16 @@ class KeyPadComponent extends React.Component {
     render() {
         return (
             <div className="button">
-                <button name="(" onClick={e => this.props.onClick(e.target.name)}>C</button>
-                <button name="CE" onClick={e => this.props.onClick(e.target.name)}>(</button>
+                <button name="C" onClick={e => this.props.onClick(e.target.name)}>C</button>
+                <button name="(" onClick={e => this.props.onClick(e.target.name)}>(</button>
                 <button name=")" onClick={e => this.props.onClick(e.target.name)}>)</button>
-                <button name="C" onClick={e => this.props.onClick(e.target.name)}>DEL</button><br />
+                <button name="DEL" onClick={e => this.props.onClick(e.target.name)}>DEL</button><br />
 
 
-                <button name="1" onClick={e => this.props.onClick(e.target.name)}>1</button>
-                <button name="2" onClick={e => this.props.onClick(e.target.name)}>2</button>
-                <button name="3" onClick={e => this.props.onClick(e.target.name)}>3</button>
-                <button name="+" onClick={e => this.props.onClick(e.target.name)}>+</button><br />
+                <button name="7" onClick={e => this.props.onClick(e.target.name)}>7</button>
+                <button name="8" onClick={e => this.props.onClick(e.target.name)}>8</button>
+                <button name="9" onClick={e => this.props.onClick(e.target.name)}>9</button>
+                <button name="/" onClick={e => this.props.onClick(e.target.name)}>/</button><br />
 
 
                 <button name="4" onClick={e => this.props.onClick(e.target.name)}>4</button>
