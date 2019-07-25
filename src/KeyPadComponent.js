140600879846a20315446236971672e56f8a1eb6
Irene Mutua <mutuamirene@gmail.com>
2019-07-25 10:56:50 +0300

    Changed the buttons

--- src/KeyPadComponent.js
+++ src/KeyPadComponent.js
@@ -5,10 +5,10 @@ class KeyPadComponent extends React.Component {
     render() {
         return (
             <div className="button">
-                <button name="(" onClick={e => this.props.onClick(e.target.name)}>(</button>
-                <button name="CE" onClick={e => this.props.onClick(e.target.name)}>CE</button>
+                <button name="(" onClick={e => this.props.onClick(e.target.name)}>C</button>
+                <button name="CE" onClick={e => this.props.onClick(e.target.name)}>(</button>
                 <button name=")" onClick={e => this.props.onClick(e.target.name)}>)</button>
-                <button name="C" onClick={e => this.props.onClick(e.target.name)}>C</button><br />
+                <button name="C" onClick={e => this.props.onClick(e.target.name)}>DEL</button><br />
 
 
                 <button name="1" onClick={e => this.props.onClick(e.target.name)}>1</button>
