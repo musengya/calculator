7f3973f03ede10272185aaf3a0f659c7be02ebc1
Irene Mutua <mutuamirene@gmail.com>
2019-07-28 16:21:39 +0300

    Modify result component

--- src/ResultComponent.js
+++ src/ResultComponent.js
@@ -7,7 +7,7 @@ class ResultComponent extends Component {
     render() {
         let { result } = this.props;
         return (
-            <div className = "">
+            <div className = "parent">
             <div className="result">
                 <p>{result}</p>
                 </div>
